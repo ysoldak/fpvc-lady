db60352cf75fb3d225eb0dc91acd317331e107b3
Yurii Soldak <ysoldak@gmail.com>
2024-07-05 16:22:10 +0200

    App Version

--- cmd/lady/main.go
+++ cmd/lady/main.go
@@ -7,12 +7,15 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+var Version = "0.0.0"
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "fpvc-lady"
 
 	app.Flags = getFlags()
 	app.Action = commentAction
+	app.Version = Version
 
 	if err := app.Run(os.Args); err != nil {
 		log.Fatalf("error: %s\n", err.Error())
