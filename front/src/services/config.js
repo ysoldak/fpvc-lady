16aadca844b6c236ed1a493d0e8c802924e990e1
zborecque <zborecque@gmail.com>
2024-09-19 20:05:33 +0200

    demo mode somehow working on public server

--- front/src/services/config.js
+++ front/src/services/config.js
@@ -1,5 +1,6 @@
 import Axios from 'axios'
 const apiUrl = process.env.REACT_APP_MOCK_API_URL
+const demoMode = process.env.REACT_APP_DEMO_MODE
 
 Axios.defaults.withCredentials = true;
 
@@ -12,10 +13,13 @@ const config = {
 const configService = {
   
   getConfig() {
-    return Axios.get(apiUrl + "config", {}, config)
+    return Axios.get(apiUrl + (demoMode.toString() === '1' ? '' : "config"), {}, config)
   },
   
   updateConfig(label, value) {
+    if (demoMode.toString() === '1') {
+      return Axios.get(apiUrl + (demoMode.toString() === '1' ? '' : "config"), {}, config)
+    }
     let updateObj = {}
     updateObj[label] = value
     return Axios.patch(apiUrl + "config", JSON.stringify(updateObj), config)
