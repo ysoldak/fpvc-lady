de59bb94cc46fcfb216aaf2d15abb5b12c156857
zborecque <zborecque@gmail.com>
2024-12-22 16:48:59 +0100

    adjusted build method, readme updated

---
+++ gui/move_build.js
@@ -0,0 +1,9 @@
+var pjson = require('./package.json');
+var fs = require('fs-extra')
+
+var oldPath = './build/'
+
+fs.move(oldPath, pjson.buildDir, function (err) {
+  if (err) throw err
+  console.log('Successfully moved build dircetory.')
+})
